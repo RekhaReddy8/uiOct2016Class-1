b309ccfc79a171c82fa92f1139db97234968a7b0
Rekha <youremail@domain.com>
2016-11-23 10:39:36 -0500

    javascript primitivedatatypes

--- javascript/11_22_2016/rekha/primitivedatatypes.js
+++ javascript/11_22_2016/rekha/primitivedatatypes.js
@@ -1,3 +1,12 @@
 /**
  * Created by rekha reddy on 11/22/2016.
  */
+
+var x='rekha';
+var y=5.6;
+var deposite=true;
+var checking=null;
+var p=undefined;
+
+console.log(x);
+console.log(y,desposite);
